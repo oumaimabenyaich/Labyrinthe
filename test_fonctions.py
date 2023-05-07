9ebd8c3ae3ddc1f5560c663f070929b44abdbdaf
Oumaima Benyaich <21237@ecam.be>
2023-05-07 22:04:39 +0200

    test_fonction

---
+++ test_fonctions.py
@@ -0,0 +1,42 @@
+import pytest
+import fonctions
+
+def test_moveAEnvoyer():
+    assert fonctions.moveAEnvoyer(1,2,3) == 
+
+
+def jeuDuCoupTest():
+    assert fonctions.jeuDuCoupTest(  ) == 
+
+
+def typeTile():
+    assert fonctions.typeTile(  ) == 
+
+
+def pionAUnePorte():
+    assert fonctions.pionAUnePorte(  ) == 
+
+
+def recreerLaMap():
+    assert fonctions.recreerLaMap(  ) == 
+
+
+def ouEstLeTresor():
+    assert fonctions.ouEstLeTresor(  ) == 
+
+
+def jeuDuCoup():
+    assert fonctions.jeuDuCoup(  ) ==
+
+
+
+
+
+
+
+
+
+
+
+
+
